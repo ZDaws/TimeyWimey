8def7245a7a68dc859f169a2966d0c4a3ed8ac81
ZDaws <zach.a.dawson@gmail.com>
2016-01-19 10:36:08 -0600

    Initial Commit

---
+++ TimeyWimey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeyWimey
+//
+//  Created by DAWSON, ZACHARIAH on 1/19/16.
+//  Copyright © 2016 Frands. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
