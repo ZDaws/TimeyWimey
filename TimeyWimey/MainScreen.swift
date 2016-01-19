6bbefbe1c0d74d0b0d9ceee065e8f81094d47f67
KUSKE JOEL <812195@ehs-s-itc-22.local>
2016-01-19 10:57:09 -0600

    Added main view controller class.

---
+++ TimeyWimey/MainScreen.swift
@@ -0,0 +1,25 @@
+//
+//  MainScreen.swift
+//  TimeyWimey
+//
+//  Created by KUSKE, JOEL on 1/19/16.
+//  Copyright © 2016 Frands. All rights reserved.
+//
+
+import UIKit
+
+class MainScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        //Do stuff
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+
+}
