2f8f0efa228bcb86b2ceee0a8be9ae4d39771e7c
CHERMAK ZACHARY <734269@ehs-s-itc-20.local>
2016-01-20 10:34:18 -0600

    Created an EventClass file for creating events.

---
+++ TimeyWimey/EventClass.swift
@@ -0,0 +1,16 @@
+//
+//  EventClass.swift
+//  TimeyWimey
+//
+//  Created by CHERMAK, ZACHARY on 1/20/16.
+//  Copyright © 2016 Frands. All rights reserved.
+//
+
+import Foundation
+
+
+Class EventClass {
+    
+    
+    
+}
