ea02e6f76b080a2d3748cc55f32634fbbd738b8d
CHERMAK ZACHARY <734269@ehs-s-itc-20>
2016-02-19 10:35:31 -0600

    adjusted cvs in runner class and added cvs class

---
+++ TimeyWimey/CSV .swift
@@ -0,0 +1,30 @@
+//
+//  CSV .swift
+//  TimeyWimey
+//
+//  Created by CHERMAK, ZACHARY on 2/17/16.
+//  Copyright © 2016 Frands. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CSV {
+    
+    
+    let fileName = "Events.csv"
+    let StartString = "Name,End Time,Lap\n,"
+    
+    func createFile() {
+        
+      // let tmpDir = NSTemporaryDirectory()
+      //let path = tmpDir.stringByAppendingPathComponent(fileName)
+       //let contentsOfFile = StartString
+    }
+    
+    
+    
+    
+    
+}
