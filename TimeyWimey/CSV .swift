745ee884f5ab2ce43a56a14f40f8e80a0a3de5e1
CHERMAK ZACHARY <734269@ehs-s-itc-20.local>
2016-02-23 10:33:06 -0600

    worked on csv class

--- TimeyWimey/CSV .swift
+++ TimeyWimey/CSV .swift
@@ -15,15 +15,30 @@ class CSV {
     
     let fileName = "Events.csv"
     let StartString = "Name,End Time,Lap\n,"
+    let tmpDir: NSString = NSTemporaryDirectory() as String
+    let contentsOfFile: String
+    
     
     func createFile() {
         
-      // let tmpDir = NSTemporaryDirectory()
-      //let path = tmpDir.stringByAppendingPathComponent(fileName)
-       //let contentsOfFile = StartString
+       let path = tmpDir.stringByAppendingPathComponent(fileName)
+
+        
+        do {
+            try contentsOfFile.writeToFile(path, atomically: true, encoding: NSUTF8StringEncoding)
+            print("File Events.csv created at tmp directory")
+        } catch {
+            
+            print("Failed to create file")
+            print("\(error)")
+        }
+        
+    
     }
     
-    
+    func fill() {
+        
+    }
     
     
     
